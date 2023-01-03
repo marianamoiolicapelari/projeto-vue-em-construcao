298efa34f782c885129afb3e56808a6f5edb8c42
Mariana M.C <marianamoioli@gmail.com>
2023-01-03 09:46:01 -0300

    Finished layout

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="space-y-2">
+    <TodoItem />
+  
+  </div>
+</template>
+<script>
+import TodoItem from "@/components/TodoItem";
+export default {
+  name: "TodoItem",
+  components: { TodoItem },
+};
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/index.html",
-    "./src/**/*.{html,js,vue}"
-  ],
+  content: ["./public/index.html", "./src/**/*.{html,js,vue}"],
 
   theme: {
     extend: {},
